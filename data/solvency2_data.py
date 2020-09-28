5a40df22c15a5f8cf1ae0a223a03220effcc5562
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2020-09-28 16:39:49 +0200

    taxonomies deleted from repo

--- data/solvency2_data.py
+++ data/solvency2_data.py
@@ -27,16 +27,15 @@ url_inst = 'https://dev.eiopa.europa.eu/Taxonomy/Full/2.4.0/S2/'
 path_zipfiles = join('downloaded files')
 
 name_zipfile_taxo = 'EIOPA_SolvencyII_XBRL_Taxonomy_2.4.0_with_external_hotfix.zip'
-path_zipfile_taxo = join('taxonomy', 'arelle', 'cache', 'http')
-path_zipfile_taxo_2 = join('taxonomy', 'arelle', 'cache', 'https')
-extension_taxo = join('EIOPA_SolvencyII_XBRL_Taxonomy_2.4.0_Hotfix_with_external')
 
 name_zipfile_inst = 'EIOPA_SolvencyII_XBRL_Instance_documents_2.4.0.zip'
 path_zipfile_inst = join('.', 'instances')
 extension_inst =  join('EIOPA_SolvencyII_XBRL_Instance_documents_2.4.0', 'random')
 
+path_zipfile_taxo = join('.', 'taxonomies')
+
 @click.command()
-@click.option('--delete_old_files', prompt = "Delete old files? (Y/N)", default='Y', help='Delete files in existing taxonomy directory')
+@click.option('--delete_old_files', prompt = "Delete old files? (Y/N)", default='N', help='Delete files in existing taxonomy directory')
 
 def main(delete_old_files):
     logger = logging.getLogger(__name__)
@@ -44,16 +43,13 @@ def main(delete_old_files):
     logger.info("Platform %s", str(_platform))
 
     # Delete content if there is content
-    if delete_old_files == 'Y':
-        if os.path.exists(path_zipfile_taxo):
-            logger.info("Deleting files in %s (this might take a while)", path_zipfile_taxo)
-            shutil.rmtree(winapi_path(path_zipfile_taxo))
-        if os.path.exists(path_zipfile_taxo_2):
-            logger.info("Deleting files in %s (this might take a while)", path_zipfile_taxo_2)
-            shutil.rmtree(winapi_path(path_zipfile_taxo_2))
-        if os.path.exists(path_zipfile_inst):
-            logger.info("Deleting files in %s", path_zipfile_inst)
-            shutil.rmtree(path_zipfile_inst)
+    # if delete_old_files == 'Y':
+    #     if os.path.exists(path_zipfile_taxo):
+    #         logger.info("Deleting files in %s (this might take a while)", path_zipfile_taxo)
+    #         shutil.rmtree(winapi_path(path_zipfile_taxo))
+    #     if os.path.exists(path_zipfile_inst):
+    #         logger.info("Deleting files in %s", path_zipfile_inst)
+    #         shutil.rmtree(path_zipfile_inst)
 
     if not os.path.exists(path_zipfile_taxo):
         make_path(path_zipfile_taxo)
@@ -66,18 +62,21 @@ def main(delete_old_files):
     logger.info("Moving example instance files to %s", path_zipfile_inst)
     move_files(join(path_zipfiles, extension_inst), path_zipfile_inst)
 
-    logger.info("Extracting taxonomy from %s", url_taxo)
-    extract(url_taxo, name_zipfile_taxo, path_zipfiles)
+    if not os.path.isfile(join(path_zipfile_taxo, name_zipfile_taxo)):
+        if os.path.isfile(join(path_zipfiles, name_zipfile_taxo)):
+            logger.info('Zip file %s exists, using this one' % str(name_zipfile_taxo))
+        else:
+            logger.info('Zip file %s does not exists, downloading from Internet' % str(name_zipfile_taxo))
+            r = requests.get(url_inst + name_zipfile_taxo)
+            output = open(join(path_zipfiles, name_zipfile_taxo), "wb")
+            output.write(r.content)
+            output.close()
 
-    logger.info("Moving taxonomy files to %s", path_zipfile_taxo)
-    move_files(join(path_zipfiles, extension_taxo), path_zipfile_taxo)
+        logger.info("Moving taxonomy to %s", path_zipfile_taxo)
+        shutil.move(join(path_zipfiles, name_zipfile_taxo), path_zipfile_taxo)
+    else:
+        logger.info("taxonomy already exists")
 
-    # correct eiopa deactivations error
-    shutil.copytree(join(path_zipfile_taxo, 'dev.eiopa.europa.eu'), join(path_zipfile_taxo_2, 'dev.eiopa.europa.eu'))
-
-    logger.info("Cleaning up files in %s", path_zipfiles)
-    shutil.rmtree(winapi_path(join(path_zipfiles, 'EIOPA_SolvencyII_XBRL_Taxonomy_2.4.0_Hotfix_with_external')))
-    shutil.rmtree(winapi_path(join(path_zipfiles, 'EIOPA_SolvencyII_XBRL_Instance_documents_2.4.0')))
 
     logger.info("Thank you for waiting!")
 
